e40178ff0ad925c45714a9a527e4bdd411da451a
Hoanganh270400 <63353923+Hoanganh270400@users.noreply.github.com>
2020-05-20 21:02:27 +0700

    Add files via upload

---
+++ prac8/silver_service_taxi_test.py
@@ -0,0 +1,14 @@
+"""
+Github link: https://github.com/Hoanganh270400/CP1404-practicals
+"""
+from SilverServiceTaxi import SilverServiceTaxi
+
+
+def main():
+    my_silver_taxi = SilverServiceTaxi("Silver_Taxi_1", 100, 2)
+    my_silver_taxi.drive(18)
+    print("Taxi's current fare: ${}".format(my_silver_taxi.get_fare()))
+    print(my_silver_taxi.__str__())
+
+
+main()
